f1b8b091d87d7572e7f55109ef82d01dc9de111a
emirhangecer <emirhan19072121@gmail.com>
2024-07-22 00:32:59 +0300

    new

--- ContactsApp/Anasayfa.swift
+++ ContactsApp/Anasayfa.swift
@@ -19,10 +19,12 @@ class Anasayfa: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        kisilerTableView.delegate = self
+        kisilerTableView.dataSource = self
         
         let k1 = Kisiler(kisi_id: 1, kisi_ad: "Allan", kisi_tel: "1111")
-        let k1 = Kisiler(kisi_id: 2, kisi_ad: "Saint", kisi_tel: "2222")
-        let k1 = Kisiler(kisi_id: 3, kisi_ad: "Maximin", kisi_tel: "3333")
+        let k2 = Kisiler(kisi_id: 2, kisi_ad: "Saint", kisi_tel: "2222")
+        let k3 = Kisiler(kisi_id: 3, kisi_ad: "Maximin", kisi_tel: "3333")
         
         kisilerListesi.append(k1)
         kisilerListesi.append(k2)
@@ -42,6 +44,23 @@ class Anasayfa: UIViewController {
 extension Anasayfa : UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print("Kisi Ara: \(searchText)")
+    }
+}
+extension Anasayfa : UITableViewDelegate , UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return kisilerListesi.count
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let kisi = kisilerListesi[indexPath.row]
+        
+        let hucre = tableView.dequeueReusableCell(withIdentifier: "kisilerHucre") as! KisilerHucre
+        
+        hucre.labelKisiAd.text = kisi.kisi_ad
+        hucre.labelKisiTel.text = kisi.kisi_tel
+        
+        return hucre
+            
     }
 }
 
