50995b04369e2c5e7ca7be42ddef5041a0cefd92
emirhangecer <emirhan19072121@gmail.com>
2024-07-22 00:21:09 +0300

    new

--- ContactsApp/Anasayfa.swift
+++ ContactsApp/Anasayfa.swift
@@ -14,9 +14,19 @@ class Anasayfa: UIViewController {
     
     @IBOutlet weak var kisilerTableView: UITableView!
     
+    var kisilerListesi = [Kisiler]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        
+        let k1 = Kisiler(kisi_id: 1, kisi_ad: "Allan", kisi_tel: "1111")
+        let k1 = Kisiler(kisi_id: 2, kisi_ad: "Saint", kisi_tel: "2222")
+        let k1 = Kisiler(kisi_id: 3, kisi_ad: "Maximin", kisi_tel: "3333")
+        
+        kisilerListesi.append(k1)
+        kisilerListesi.append(k2)
+        kisilerListesi.append(k3)
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
